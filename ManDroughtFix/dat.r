32e8e22b37b37b1baa1931554d9391b25452bfd5
Christopher Pearce <christopher.pearce@outlook.co.nz>
2014-12-02 13:58:41 +1300

    Working on simplifying the GIS process. So far I have axed 1 module

---
+++ ManDroughtFix/dat.r
@@ -0,0 +1,6 @@
+# For CSV loc and GIS loc make sure you provide absolute file references
+# You must only provide the file name for shp.name
+CSV.loc ="~/Studentship/ManDroughtFix/agesexdata.csv"
+GIS.loc ="~/Studentship/ManDroughtFix/GISNZ2"
+shp.name="TA2014_GV_Clipped.shp"
+title	="Age"
