ed04a522fded7c41c56ea521d1f1eac4aac650ac
cajpearce <christopher.pearce@outlook.co.nz>
2014-12-09 15:39:04 +1300

    Fix on colours in ManDroughtFix

--- ManDroughtFix/colourCreator.r
+++ ManDroughtFix/colourCreator.r
@@ -1,18 +1,20 @@
 # Creates a colour for P-values - should defo be customised
 library(scales)
 
+
+# would multiplying the P-value by (0.5 - percentage) * 2 be more relevant?
+
 num.bind = function(v1, low=0, high=1) {
 	v1[v1 < 0] = 0
 	v1[v1 > 1] = 1
 	v1
 }
 
-colour.function = function(p.values, v1, midpoint=0.5) {
-	indices = log10(1.05 - p.values)/log10(1.05)	
-	indices = num.bind(indices) * 0.9
-	logical.lower = v1 < midpoint
+colour.function = function(p.values, v1, midpoint=0.5, cutoff=0.05, offset = 0.01) {
+	indices = log10(1 + cutoff + offset - p.values)/log10(1 + cutoff + offset)	
+	indices = num.bind(indices)
 	cols = ifelse(v1 < midpoint, rgb(0,1,0,alpha=indices),rgb(1,0,0,alpha=indices))
-	cols[indices == 0] = rgb(1,1,0,alpha = 0.2)
+	cols[p.values > cutoff] = rgb(1,1,0,alpha = 0.2)
 	cols
 }
 
