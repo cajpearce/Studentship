057f9a8ece640d7535890cfe00e11c4e90df9d29
cajpearce <christopher.pearce@outlook.co.nz>
2015-01-09 16:57:52 +1300

    Almost got it working... downlaods CSVs in a weird way. calling it a day

--- TideMeasures/tide.r
+++ TideMeasures/tide.r
@@ -37,16 +37,20 @@ identifiers = levels(factor(identifiers))
 
 library(stringr)
 
+# Creates a list of the webpages where downloads are found (each IDENTIFIER has
+# its own webpage which contains the measurements
 download.locations = paste(root.link,identifiers,"/",year,"/",identifiers,sep="")
 
-
-
-top = "http://apps.linz.govt.nz/ftp/sea_level_data/AUCT/2014"
-
+# need to pad zeroes onto 1-99 using library(stringr)
 day.numbers = str_pad(1:365, 3, pad = "0")
+
+# need to paste the year onto each day (001-365)
 dates = paste(year,day.numbers,sep="")
 
+# paste together the top page of each individual identifier with the zip file
 all.downloads = lapply(download.locations, paste, "_",dates,".zip",sep="")
+
+#similar but just adds .csv (since this is what's inside each zip file
 all.csvs = lapply(identifiers,paste,"_",dates,".csv",sep="")
 
 ################################################################################
@@ -56,13 +60,34 @@ all.csvs = lapply(identifiers,paste,"_",dates,".csv",sep="")
 ################################################################################
 
 # Using code from ManDrought
+# SHOULD ONLY USE THIS ONCE - 304 MB of data, should probably save as RDS.
 
 read.csv.from.zip = function(file.zip,csv.name) {
-  temp.zip = tempfile()
-  dir.create(temp.zip)
-  unzip(file.zip,exdir=temp.zip)
-  measurements = read.csv(csv.name)
-  unlink(temp.zip)
-  measurements
+	temp = tempfile()
+	measurements = data.frame()
+
+	download.file(file.zip,temp,quiet=TRUE)
+	
+	# if it cant be unzipped it just returns an empty dataframe now :D
+	tryCatch(assign("measurements",read.csv(unz(temp,csv.name)),
+			error=function(e) {print(paste("ERROR:",csv.name))},warning=function(w) {})
+
+	unlink(temp)
+	
+	measurements
 }
 
+vec.read = Vectorize(read.csv.from.zip)
+
+check.cache = function(name="alltides.rds") {
+	temp = ""
+	if (file.exists(name)) {
+		temp = readRDS(name)
+	} else {
+		temp = mapply(vec.read, all.downloads,all.csvs,SIMPLIFY=FALSE)
+		#saveRDS(temp,name)
+	}
+	temp
+}
+
+all.tide.measures = check.cache()
