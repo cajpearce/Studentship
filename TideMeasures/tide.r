49a52abd3837273c1cf5508c75993354e01673a5
cajpearce <christopher.pearce@outlook.co.nz>
2015-01-14 13:50:41 +1300

    Trying to figure out high tides - is there a regular period

--- TideMeasures/tide.r
+++ TideMeasures/tide.r
@@ -134,6 +134,69 @@ ymax = max(new.first.set$height,new.second.set$height)
 plot(new.first.set$datetime,new.first.set$height,ylim = c(ymin,ymax),type="l")
 lines(new.second.set$datetime,new.second.set$height,col="red")
 
+
+################################################################################
+# 	CAN SPLIT HERE
+################################################################################
+#	but won't :)
+################################################################################
+library(zoo)
+z1 = zoo (new.first.set$height, new.first.set$datetime)
+z2 = zoo (new.second.set$height, new.second.set$datetime)
+m = merge(z1, z2)
+
+
+#plot(m)
+
+# try to figure out the max height
+get.extreme.tides = function(tide.set) {
+	# TODO: include functionality for testing to make sure that the ends of 
+	# the dataset don't reference high/low tides
+
+
+	#reference
+	ht = tide.set$height
+	dt = tide.set$datetime
+
+	# rough middle point
+	middle = mean(ht)
+
+	# to store dates
+	all.extremes = .POSIXct(character(0))
+
+	# are we currently above middle
+	above.middle = ht[1] > middle
+	
+	# what are our current extremes
+	curr.extreme = ht[1]
+	curr.extreme.date = dt[1]
+
+	for (i in 2:length(dt)) {
+		curr = ht[i]
+
+		if ((curr > middle & !above.middle) | (curr < middle & above.middle)) {
+			# at the switch, reset everything
+			all.extremes[length(all.extremes) + 1] = as.POSIXct(curr.extreme.date)
+			curr.extreme.date = dt[i]
+			curr.extreme = curr
+			above.middle = curr > middle
+		}
+		if ((above.middle & curr > curr.extreme) | (!above.middle & curr < curr.extreme)) {
+			curr.extreme = curr
+			curr.extreme.date = dt[i]
+		}
+	}
+	all.extremes
+}
+
+
+abline(v=get.extreme.tides(new.first.set))
+# zooreg(1:5,start=as.POSIXct("2014-01-01 01:00:00"))
+# something to think about
+
+
+
+
 # thoughts: 
 #library("TTR")
 
