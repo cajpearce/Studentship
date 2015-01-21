09299bc5a967c4ee59640483271d2633beb03448
cajpearce <christopher.pearce@outlook.co.nz>
2015-01-22 11:48:34 +1300

    Working on PyGlue - trying to understand XML

--- PyGlue/module.py
+++ PyGlue/module.py
@@ -1,17 +1,46 @@
-try:
-    import xml.etree.cElementTree as ET
-except ImportError:
-    import xml.etree.ElementTree as ET
+from lxml import etree
 
-tree = ET.ElementTree(file="test.xml")
+# read the ElementTree
+tree = etree.parse("test.xml")
+
+#assigns the root element
 root = tree.getroot()
 
+# assigns the namespace dictionary to NSMAP
+NSMAP = root.nsmap
 
-for elem in tree.iter():
-	print elem.tag, elem.attrib
+# assigns our sole namespace to namespace
+namespace = "{" + NSMAP[None] + "}"
 
-#{http://www.openapi.org/2014/}platform {'name': 'python'}
-#{http://www.openapi.org/2014/}description {}
-#{http://www.openapi.org/2014/}output {'type': 'internal', 'name': 'df'}
-#{http://www.openapi.org/2014/}source {'ref': 'inflation.py'}
+# prints out the XML
+print etree.tostring(root, pretty_print=True)
 
+
+# prints out all the sub-element tags
+for child in root:
+	print child.tag
+
+# prints out whether the root element is an element
+print etree.iselement(root)
+
+# prints if the root has sub elements
+if len(root):
+	print("The root element has children")
+
+
+# testing out the abilities to detect the parent
+print root is root[0].getparent()
+
+# testing out navigation
+print root[0] is root[1].getprevious() and root[1] is root[0].getnext()
+
+# prints out root tags (stuff like version)
+print "ROOT KEYS: " +str(root.keys())
+
+# ALTERNATIVE
+attributes_of_root = root.attrib
+
+print "DIRECT KEYS: " + str(attributes_of_root)
+
+# prints out the version hopefully
+print "VERSION: " + str(root.get(root.keys()[0]))
