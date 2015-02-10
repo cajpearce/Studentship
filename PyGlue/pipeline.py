d95ee990b08dbc53ecbd0fbc45da6f8de9ed1d10
cajpearce <christopher.pearce@outlook.co.nz>
2015-02-10 15:36:16 +1300

    Hopefully successfully edited it so it makes more sense

--- PyGlue/pipeline.py
+++ PyGlue/pipeline.py
@@ -1,21 +1,22 @@
 from lxml import etree
-from warnings import warn
 from topological_sort import sort_topologically
 import os
-#import module
 
-class Pipeline:
+class GetXMLStuff(object):
 	def __init__(self, xml_file):
+		if not os.path.isfile(xml_file):
+			raise IOError("You have provided an invalid XML file.")
 		self.xml_file = xml_file
 
 		self.setup_tree()
 		self.setup_namespace()	
 		self.setup_root()
 
-		self.parse_information_from_xml()
-		self.read_in_modules()
+	def search_inside_tag(self, tag, attribute):
+		return self.root.find(self.namespace + tag).get(attribute)
 
-		self.running_order = self.collapse_list_of_sets_into_module_order()
+	def __str__(self):
+		return self.xml_file
 
 	def setup_tree(self):
 		self.tree = etree.parse(self.xml_file)
@@ -33,54 +34,39 @@ class Pipeline:
 		self.root = self.tree.getroot()
 		return self.root
 
-	# above completed		
 
-	def collapse_list_of_sets_into_module_order(self):
-		return [m for subset in self.module_order for m in subset]
+
+class Pipeline(GetXMLStuff):
+	def __init__(self, xml_file):
+		GetXMLStuff.__init__(self, xml_file)
+
+		self.parse_information_from_xml()
+
+		# self.module_order
+		# self.modules
+		# self.all_input_pipes
+		# self.all_output_pipes
 		
-	def run_pipeline(self):
-		has_been = list()
-		#TODO
-		for m in self.running_order:
-			has_been.append(m)
+
+	def collapse_module_order_list(self, module_order):
+		return [m for subset in module_order for m in subset]
 			
-			what_i_need_inputs_from = m.which_modules_are_providing_inputs()
-			
-			for i in what_i_need_inputs_from:
-				pass
-				#TODO
-						
-
-
-	def parse_information_from_xml(self):
-		self.components = self.root.findall(self.namespace+"component")
-		self.pipes = self.root.findall(self.namespace + "pipe")
-
-
-		self.components_dict = self.create_component_dictionary()
-		graph, self.all_input_pipes, self.all_output_pipes = self.create_graphs()		
-		self.module_order = sort_topologically(graph)
-		self.module_order.reverse()
-
-		# NOTE ON VARIABLE_PIPE_pipes
-		# IT IS 'OUTPUT' POINTING TO 'INPUT'
-			
-	
-	def read_in_modules(self):
-		self.modules = {}
-		for c in self.components:
+	def read_in_modules(self, components):
+		modules = {}
+		for c in components:
 			if c.get('type') == 'module':
 				module_file_name = c.get('ref')
-				self.modules[module_file_name] = Module(module_file_name, 
-				dict(self.all_input_pipes), dict(self.all_output_pipes))
+				print module_file_name
+				modules[module_file_name] = Module(module_file_name)
 				# creates new dictionaries
+		
+		return modules
 
-
-	def create_component_dictionary(self):
+	def component_dictionary(self, components):
 		# creates a dictionary
 		# really simply, just maps m1 to module1.xml
 		ret_dict = dict()
-		for component in self.components:
+		for component in components:
 			if component.get('type') == "module":
 				key = component.get('name')
 				reference = component.get('ref')
@@ -88,21 +74,50 @@ class Pipeline:
 				ret_dict[key] = reference
 		return ret_dict
 
-	def create_graphs(self):
+	def parse_information_from_xml(self):
+		components = self.root.findall(self.namespace+"component")
+		pipes = self.root.findall(self.namespace + "pipe")
+		
+		self.modules = self.read_in_modules(components)
+
+		graph, self.all_input_pipes, self.all_output_pipes = self.create_graph(pipes, components)	
+
+		self.set_module_pipes(self.modules)		
+
+		order = sort_topologically(graph)
+		order.reverse()
+		
+		self.module_order = self.collapse_module_order_list(order)
+
+	def set_module_pipes(self, modules):
+		for xml_file, m in modules.items():
+			m.set_pipes(dict(self.all_input_pipes), 
+				dict(self.all_output_pipes))
+
+	def create_graph(self, pipes, components):
 		graph = dict()
 		all_input_pipes = dict()
 		all_output_pipes = dict()
 
-		for pipe in self.pipes:
+		ref_dict = self.component_dictionary(components)
+		for pipe in pipes:
+			# Elements
 			start = pipe.find(self.namespace + "start")
 			end = pipe.find(self.namespace + "end")
-		
+			
+			# m1, m2, etc.
 			o_component = start.get("component")
 			i_component = end.get("component")
 			
-			o_component = self.components_dict[o_component]
-			i_component = self.components_dict[i_component]
-
+			# xml file names
+			o_component = ref_dict[o_component]
+			i_component = ref_dict[i_component]
+			
+			# actual module objects
+			o_component = self.modules[o_component]
+			i_component = self.modules[i_component]
+			
+	
 			o_variable = start.get("output")
 			i_variable = end.get("input")
 
@@ -119,77 +134,36 @@ class Pipeline:
 		return graph, all_input_pipes, all_output_pipes
 
 
-class Module:
-	def __init__(self, xml_file, input_pipes, output_pipes):
-		if not os.path.isfile(xml_file):
-			raise IOError("That is not a file you fucking jerk.")
-
-		self.xml_file = xml_file
-
-		self.setup_tree()
-		self.setup_namespace()	
-		self.setup_root()
+class Module(GetXMLStuff):
+	def __init__(self, xml_file):
+		GetXMLStuff.__init__(self, xml_file)
 
 		self.parse_information_from_xml()
-
+	
+	def set_pipes(self, input_pipes, output_pipes):
+		# TODO MUST USE THIS
+		#		
+	
 		self.input_pipes = self.filter_pipes(input_pipes)
 		self.output_pipes = self.filter_pipes(output_pipes)
-
-		# makes them specific to Modules!
-		self.filter_pipes(self.input_pipes)
-		self.filter_pipes(self.output_pipes)		
-		
 		
 	def filter_pipes(self, pipes):
 		new_pipes = {}
 		for key, value in pipes.items():
-			if key[0] == self.xml_file:
+			if key[0] is self:
 				new_pipes[key] = value
 		return new_pipes
 
-	def setup_tree(self):
-		self.tree = etree.parse(self.xml_file)
-
-		return self.tree
-
-	def setup_namespace(self):
-		temp_namespace = self.tree.getroot().nsmap
-		#formats it properly and assigns it to self
-		self.namespace = "{" + temp_namespace[None] + "}"
-
-		return self.namespace
-
-	def setup_root(self):
-		self.root = self.tree.getroot()
-		return self.root
-
 	def parse_information_from_xml(self):
-		self.source_file = self.search_inside_tag("source","ref")
+		self.run_file = self.search_inside_tag("source","ref")
 		self.platform = self.search_inside_tag("platform","name")
 		
 		# These are still elements tho
 		self.inputs = self.root.findall(self.namespace + "input")
 		self.outputs = self.root.findall(self.namespace + "output")
 		
-		self.input_types, self.input_names = self.create_types_and_names(self.inputs)
-		self.output_types, self.output_names = self.create_types_and_names(self.outputs)
-
-		
-
-	def search_inside_tag(self, tag, attribute):
-		return self.root.find(self.namespace + tag).get(attribute)
-	
-	def __str__(self):
-		return etree.tostring(self.tree,pretty_print=True)
-
-	def create_types_and_names(self, elements):
-		types = []
-		names = []
-		for e in elements:
-			types.append(e.get("type"))
-			names.append(e.get("name"))
-
-		return types, names
+		#self.input_types, self.input_names = self.create_types_and_names(self.inputs)
+		#self.output_types, self.output_names = self.create_types_and_names(self.outputs)
 
 
 	def which_modules_are_providing_inputs(self):
@@ -227,7 +201,14 @@ class Module:
 		self.save_locals = locals()
 				
 
-	
+	#def create_types_and_names(self, elements):
+	#	types = []
+	#	names = []
+	#	for e in elements:
+	#		types.append(e.get("type"))
+	#		names.append(e.get("name"))
+#
+#		return types, names
 
 
 simple_pipe  = Pipeline("pipe.xml")
